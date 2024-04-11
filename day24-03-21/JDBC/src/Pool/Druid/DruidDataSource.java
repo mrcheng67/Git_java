8565eff2d819a5c319495e3315412c42c64715ee
cheng <2363700062@qq.com>
2024-04-11 14:57:36 +0800

    删除test

--- day24-03-21/JDBC/src/Pool/Druid/DruidDataSource.java
+++ day24-03-21/JDBC/src/Pool/Druid/DruidDataSource.java
@@ -5,6 +5,8 @@ import com.alibaba.druid.pool.DruidDataSourceFactory;
 import javax.sql.DataSource;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Properties;
 
 public class DruidDataSource {
@@ -18,13 +20,18 @@ public class DruidDataSource {
         DataSource dataSource = DruidDataSourceFactory.createDataSource(properties);
         //获取获取连接
         Connection conn = dataSource.getConnection();
+        ResultSet rs = conn.prepareStatement("select * from bank").executeQuery();
         //判断是否连接成功
         if (conn!= null){
             System.out.println("数据库连接成功");
+            while(rs.next()){
+                System.out.println(rs.getString("name"));
+            }
         }else {
             System.out.println("数据库连接失败");
         }
         //释放资源
+        rs.close();
         conn.close();
 
     }
